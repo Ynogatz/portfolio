26ab5429817f400a2832b818eef83c74372c62b3
ynogatz <ynogatzds@gmail.com>
2023-04-16 15:21:30 -0300

    feat: add header in projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ProjectsSection from '../components/projects-session';
+import Head from "next/head";
+import Header from "../components/header";
 
 const Projects = () => {
     return (
         <div>
+            <Head>
+                <title>Ygor Nogatz - Portfólio</title>
+            </Head>
+            <Header />
+
             <h1>Projetos</h1>
             <ProjectsSection />
         </div>
