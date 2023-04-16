925938923523abf4b882a5eb33e5ab5a91b426e1
ynogatz <ynogatzds@gmail.com>
2023-04-16 14:31:12 -0300

    feat: add first project

--- components/projects-session.js
+++ components/projects-session.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import ProjectCard from './project-card';
 import styles from './projects-session.module.css';
+import BackButton from "./back-button";
 
 const ProjectsSession = () => {
     const projects = [
         {
-            title: 'Projeto 1',
-            description: 'Breve descrição do Projeto 1',
-            githubLink: 'https://github.com/usuario/projeto1',
+            title: 'http-rest-errors',
+            description: 'Este é um projeto de uma lib, ela tem várias classes de erro ' +
+                'com o nome adequado, status e possui também um middleware para a manipulação ' +
+                'de erros.',
+            githubLink: 'https://github.com/Ynogatz/http-rest-errors',
         },
         {
             title: 'Projeto 2',
@@ -20,7 +23,6 @@ const ProjectsSession = () => {
 
     return (
         <section className={styles.projectsSession}>
-            <h2>Projetos</h2>
             <p className={styles.projectsDescription}>
                 Aqui você encontrará uma lista dos meus projetos. Todos estão disponíveis em repositórios públicos do GitHub, e você também encontrará uma breve descrição de cada um deles.
             </p>
@@ -29,6 +31,7 @@ const ProjectsSession = () => {
                     <ProjectCard key={index} {...project} />
                 ))}
             </div>
+            <BackButton />
         </section>
     );
 };
